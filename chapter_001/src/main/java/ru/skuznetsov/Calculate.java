d3210e5c58a2f199bb72275077c51c17b3ca1a9d
kuznetsovsergeyymail.com <kuznetsovsergey@ymail.com>
2016-11-23 19:13:57 +0300

    update

--- chapter_001/src/main/java/ru/skuznetsov/Calculate.java
+++ chapter_001/src/main/java/ru/skuznetsov/Calculate.java
@@ -1,11 +1,18 @@
-package ru.skuznetsov;
-
-public class Calculate{
 /**
-* Output Hello world string
+*Class Calculate print to console constant string 
+*<p>This class contains method wich one printing Hello World phrase</p>
+* 
+* @since 1.0
+* @author skuznetsov
+* @version 2.0
 * @param args not using on code
 */
-  public static void main(String[] args){
+
+package ru.skuznetsov;
+
+public class Calculate {
+
+  public static void main(String[] args) {
 
     System.out.println("Hello World");
   }
--- chapter_001/src/test/java/ru/skuznetsov/CalculateTest.java
+++ chapter_001/src/test/java/ru/skuznetsov/CalculateTest.java
@@ -1,21 +1,34 @@
+/**
+*<p>This class contains method wich one printing Hello World phrase</p>
+* 
+*/
+
 package ru.skuznetsov;
 
-import org.junit.Test;
-import java.io.ByteArrayOutputStream;
-import java.io.PrintStream;
 import static org.hamcrest.Matchers.is;
 import static org.junit.Assert.assertThat;
 
+import org.junit.Test;
+
+import java.io.ByteArrayOutputStream;
+
+import java.io.PrintStream;
+
 /**
-* Test to equal output from console with suggestion
-*/
-public class CalculateTest{
-	
-	@Test
-	public void whenExecuteMainThenPrintToConsole(){
-		ByteArrayOutputStream out = new ByteArrayOutputStream();
-		System.setOut(new PrintStream(out));
-		Calculate.main(null);
-		assertThat(out.toString(), is("Hello World\r\n"));
-	}
+ * @author skuznetsov
+ * @version 1.0
+ * 
+ */
+
+public class CalculateTest {
+
+  @Test
+public void whenExecuteMainThenPrintToConsole() {
+
+    ByteArrayOutputStream out = new ByteArrayOutputStream();
+    System.setOut(new PrintStream(out));
+    Calculate.main(null);
+    assertThat(out.toString(), is("Hello World\r\n"));
+    
+  }
 }
