b9c9b02b5cfe218dab968a172e4bcd40fd9be250
kuznetsovsergeyymail.com <kuznetsovsergey@ymail.com>
2016-11-23 15:33:29 +0300

    update with comments

--- chapter_001/src/main/java/ru/skuznetsov/Calculate.java
+++ chapter_001/src/main/java/ru/skuznetsov/Calculate.java
@@ -1,7 +1,10 @@
 package ru.skuznetsov;
 
 public class Calculate{
-
+/**
+* Вывод Hello world
+* @param args не используется в коде
+*/
   public static void main(String[] args){
 
     System.out.println("Hello world");
