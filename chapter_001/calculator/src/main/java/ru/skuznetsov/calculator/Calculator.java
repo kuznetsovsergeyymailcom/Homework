28b48063ddcdd1389e9ec4e314ec9974c32186a5
kuznetsovsergeyymail.com <kuznetsovsergey@ymail.com>
2016-11-24 16:48:17 +0300

    calculator with tests

---
+++ chapter_001/calculator/src/main/java/ru/skuznetsov/calculator/Calculator.java
@@ -0,0 +1,31 @@
+
+package ru.skuznetsov.calculator;
+
+
+public class Calculator {
+
+    public double result;
+
+    void add(double first, double second) {
+	result = first + second;
+    }
+
+    void substruct(double first, double second) {
+	result = first - second;
+    }
+
+    void div(double first, double second) {
+	if (second == 0)
+	    throw new IllegalArgumentException("Second argument is zero");
+	result = first / second;
+    }
+
+    void multiple(double first, double second) {
+	result = first * second;
+    }
+
+    double getResult() {
+	return result;
+    }
+
+}
---
+++ chapter_001/calculator/src/test/java/ru/skuznetsov/calculator/CalculatorTest.java
@@ -0,0 +1,51 @@
+
+package ru.skuznetsov.calculator;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+
+public class CalculatorTest {
+
+    @Test
+    public void FirstAddSecondCheckResult() {
+	Calculator calc = new Calculator();
+	calc.add(12.00, 34.00);
+	double result = calc.getResult();
+	assertEquals(46, result, 0);
+    }
+
+    @Test
+    public void FirstSubstractSecondCheckResult() {
+	Calculator calc = new Calculator();
+	calc.substruct(12.00, 34.00);
+	double result = calc.getResult();
+	assertEquals(-22, result, 0);
+    }
+
+    @Test
+    public void FirstDivideSecondCheckResult() {
+	Calculator calc = new Calculator();
+	calc.div(56.00, 8.00);
+	double result = calc.getResult();
+	assertEquals(7, result, 0);
+    }
+
+    @Test
+    public void FirstMultiplySecondCheckResult() {
+	Calculator calc = new Calculator();
+	calc.multiple(12.00, 3.00);
+	double result = calc.getResult();
+	assertEquals(36, result, 0);
+    }
+
+    /**
+     * Check throw illegal argumtn exception (dividing by zero)
+     */
+    @Test(expected = IllegalArgumentException.class)
+    public void FirstDivideSecondCheckResultExpectError() {
+	Calculator calc = new Calculator();
+	calc.div(56.00, 0.00);
+	double result = calc.getResult();
+    }
+}
