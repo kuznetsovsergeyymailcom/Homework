b78c41956da4864f2ca2419fe70221035ab8ff6d
kuznetsovsergeyymail.com <kuznetsovsergey@ymail.com>
2016-11-24 23:45:02 +0300

    maximum from two numbers update 1

--- chapter_001/maximumFromTwoNumbers/src/test/java/ru/skuznetsov/MaxTest.java
+++ chapter_001/maximumFromTwoNumbers/src/test/java/ru/skuznetsov/MaxTest.java
@@ -6,9 +6,11 @@ import static org.junit.Assert.assertEquals;
 public class MaxTest {
 
     @Test
-    public void getMaxNumber(){
+    public void getMaxNumberFromTwoNumbers(){
         Max max = new Max();
         int largerNumber = max.max(3,5);
+
         assertEquals(5,largerNumber);
     }
+    
 }
